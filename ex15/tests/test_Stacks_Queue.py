6271d013ad1e0dbc36966d0bd7280e1642809c6e
puskini33 <elenahirjoaba@yahoo.com>
2019-09-25 20:22:30 +0200

    # File and Test for Stack added

---
+++ ex15/tests/test_Stacks_Queue.py
@@ -0,0 +1,47 @@
+from Stacks_Queue import Stack
+import unittest
+
+
+class TestCase(unittest.TestCase):
+
+    def test_push(self):
+        colors = Stack()
+        colors.push('Pthalo Blue')
+        assert colors.count() == 1
+        colors.push('Ultramarine Bleu')
+        assert colors.count() == 2
+        colors.push('Magenta')
+        assert colors.count() == 3
+        colors.push('Green')
+        assert colors.count() == 4
+        colors.push('Red')
+        assert colors.count() == 5
+
+    def test_pop(self):
+        colors = Stack()
+        colors.push('Magenta')
+        colors.push('Alizarin')
+        colors.push('Blue')
+        assert colors.pop() == 'Blue'
+        assert colors.pop() == 'Alizarin'
+        assert colors.pop() == 'Magenta'
+        assert colors.pop() is None
+
+    def test_top(self):
+        colors = Stack()
+        colors.push('Blue')
+        assert colors.top_node() == 'Blue'
+        colors.pop()
+        assert colors.top_node() is None
+
+    def test_dump(self):
+        colors = Stack()
+        colors.push('Magenta')
+        colors.push('Alizarin')
+        colors.dump('before Blue')
+        colors.push('Blue')
+        colors.dump('after Blue')
+
+
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
