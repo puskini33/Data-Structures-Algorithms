e30d3e679e9e06d79f11b30113c71030eacf5efb
Elena Hirjoaba <49148004+puskini33@users.noreply.github.com>
2019-10-08 09:45:27 +0200

    # Re-added files to ex13

---
+++ ex13_SingleLinkedList/setup.py
@@ -0,0 +1,13 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'First knowledge about Single Linked Lists',
+    'author': 'Elenaa',
+    'version': '0.2',
+    'name': 'Single_Linked_Lists_Project'
+}
+
+setup(**config)
