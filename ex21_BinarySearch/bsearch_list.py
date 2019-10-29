eaacbb00d4a5e3d4b46da069d82892aa68474bed
Elena Hirjoaba <49148004+puskini33@users.noreply.github.com>
2019-10-29 12:09:09 +0100

    # fix bug
    (find_number returns None if value nonexistent)

--- ex21_BinarySearch/bsearch_list.py
+++ ex21_BinarySearch/bsearch_list.py
@@ -4,19 +4,29 @@
 def _find_number(x, m, manual_list):
     if x == manual_list[m]:
         return x
+    elif x != manual_list[m] and len(manual_list) == 1:
+        x = None
+        return x
     elif x > manual_list[m]:  # if the number is bigger than value at index m
         new_list = manual_list[m+1:]
-        m = len(new_list) // 2
-        _find_number(x, m, new_list)
+        if len(new_list) % 2 == 0:
+            m = len(new_list) // 2 - 1
+        else:
+            m = len(new_list) // 2
+        return _find_number(x, m, new_list)
     elif x < manual_list[m]:  # if the number is smaller than value at index m
-        new_list = manual_list[:m-1]
-        m = len(new_list) // 2
-        _find_number(x, m, new_list)
+        new_list = manual_list[:m]
+        if len(new_list) % 2 == 0:
+            m = len(new_list) // 2 - 1
+        else:
+            m = len(new_list) // 2
+        return _find_number(x, m, new_list)
 
-    return x
 
-
-def find_number(x, manual_list):
-    m = len(manual_list) // 2
-    found = _find_number(x, m, manual_list)
+def find_number(x, man_list):
+    if len(man_list) % 2 == 0:
+        m = len(man_list) // 2
+    else:
+        m = len(man_list) // 2 + 1
+    found = _find_number(x, m, man_list)
     return found
--- ex21_BinarySearch/tests/test_bsearch_list.py
+++ ex21_BinarySearch/tests/test_bsearch_list.py
@@ -5,7 +5,8 @@ import unittest
 class TestCase(unittest.TestCase):
 
     def test_find_number(self):
-        manual_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
-        assert find_number(19, manual_list) == 19
-        assert find_number(1, manual_list) == 1
-        assert find_number(13, manual_list) == 13
+        man_list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+        assert find_number(19, man_list) == 19
+        assert find_number(1, man_list) == 1
+        assert find_number(13, man_list) == 13
+        assert find_number(23, man_list) is None
