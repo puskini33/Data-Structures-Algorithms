ea0f52efb2207a147d1b739ed336a4a2fb96ddfc
Elena Hirjoaba <49148004+puskini33@users.noreply.github.com>
2019-11-17 20:40:38 +0100

    # add code Finite State Machine

---
+++ ex30_FiniteStateMachine/test_socket_fsm.py
@@ -0,0 +1,10 @@
+from fsm import SocketFSM
+
+
+def test_basic_connection():
+    fsm = SocketFSM()
+    script = ["connect", "accept", "read", "read", "write", "close", "connect"]
+
+    for event in script:
+        print(event, ">>>", fsm)
+        fsm.handle(event)
