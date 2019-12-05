56ee046a42a05287b1fd456853063fd58b5b2457
puskini33 <elenahirjoaba@yahoo.com>
2019-12-05 21:03:04 +0100

    Add old files of Parser and new file ex34a.py

---
+++ ex34_Analyzers/ex34a.py
@@ -0,0 +1,85 @@
+"""
+1. Give the PunyPyAnalyzer a parse tree, which are all Grammar Production Classes
+2. Also give the PunyPyAnalyzer a state world(i.e., PunyPyWorld)"""
+
+
+class Production(object):  # a Production class that all of my grammar productions will inherit from
+    def analyze(self, world):  # world is the PunyPyWorld state
+        """All grammar Productions inherit from here.
+        Implement analyzer here"""
+
+
+class FuncCall(Production):
+
+    def __init__(self, name, params: 'Params Production Class'):
+        self.name = name
+        self.params = params
+
+    def analyze(self, world):
+        print("> FuncCall: ", self.name)
+        self.params.analyze(world)
+
+
+class Parameters(Production):
+
+    def __init__(self, expressions):
+        self.expressions = expressions
+
+    def analyze(self, world):
+        print(">> Parameters: ")
+        for expr in self.expressions:
+            expr.analyze(world)
+
+
+class Expr(Production):
+    # it goes to Production.analyze I think
+    pass
+
+
+class IntExpr(Expr):
+    def __init__(self, integer):
+        self.integer = integer
+
+    def analyze(self, world):
+        print(">>>> IntExpr: ", self.integer)
+
+
+class AddExpr(Expr):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
+
+    def analyze(self, world):
+        print(">>> AddExpr: ")
+        self.left.analyze(world)
+        self.right.analyze(world)
+
+
+class PunyPyWorld(object):  # state of the world
+
+    def __init__(self, variables):
+        self.variables = variables
+        self.functions = {}
+
+
+class PunyPyAnalyzer(object):   # take a parse tree and the world state and make the grammar productions run
+    """Makes all the Grammar Productions run"""
+    def __init__(self, parse_tree, world):
+        self.parse_tree = parse_tree
+        self.world = world  # PunyPyWorld object
+
+    def analyze(self):
+        for node in self.parse_tree:  # each node is a Grammar Production Rule
+            node.analyze(self.world)  # call each GP Class's analyze(SELF.WORLD) method
+
+
+variables = {}
+world = PunyPyWorld(variables)
+# simulate hello(10 + 20)
+script = [FuncCall('hello',
+                Parameters([
+                    AddExpr(IntExpr(10), IntExpr(20))
+                ]))]  # modify the indent to create this parse tree
+
+analyzer = PunyPyAnalyzer(script, world)
+analyzer.analyze()
---
+++ ex34_Analyzers/runner.py
@@ -0,0 +1,119 @@
+from parser_format import Parser
+from Scanner import Scanner
+from pprint import pprint
+import Grammar_Production_Results as Rule
+
+
+class Runner(object):
+
+    def __init__(self, local_parser):
+        """Algorithm uses left most derivation to do the syntax analysis of the list of tokens from Scanner and outputs
+        a parse tree."""
+        self.local_parser = local_parser  # the Parser object is passed
+
+    def root(self):
+        """root = *(funccal / funcdef)"""
+        first = self.local_parser.peek()  # peek at first element in the list_tokens of local scanner
+
+        if first == 'DEF':  # if the token list starts with DEF
+            return self.function_definition()  # it is a function definition; call function
+        elif first == 'INDENT':  # if the next element is NAME
+            self.local_parser.match('INDENT')
+            return self.func_body()
+        elif first == 'NAME':
+            return self.function_call(first)
+
+    def function_definition(self):
+        """funcdef = DEF name LPAREN params RPAREN COLON body
+        I ignore body for this example because that is hard.
+        I mean, so you can learn how to do it."""
+
+        self.local_parser.skip('DEF')  # discard def
+
+        name = self.local_parser.match('NAME')  # matches NAME in the code
+
+        self.local_parser.match('LPAREN')  # matches LPAREN in the code
+
+        params = self.parameters()  # list of parameters
+
+        self.local_parser.match('RPAREN')  # matches RPAREN
+
+        self.local_parser.match('COLON')  # matches COLON
+
+        return Rule.FuncDef(name, params)
+
+    def parameters(self):
+        """params = expression *(COMMA expression)"""
+        params = []
+        start = self.local_parser.peek()  # peek at current element
+        while start != 'RPAREN':  # while start is not RPAREN
+            params.append(self.expression())  # append first parameter, append second parameter
+            start = self.local_parser.peek()  # peek at next element
+
+            if start != 'RPAREN':  # if RPAREN is not reached yet, so there must be another parameter
+                self.local_parser.match('COMMA')  # match COMMA and pop it
+
+        return Rule.Parameters(params)  # return parameters
+
+    def function_call(self, name):
+        """funccall = name LPAREN params RPAREN"""
+
+        name = self.local_parser.match('NAME')  # peek at the next element
+
+        self.local_parser.match('LPAREN')  # match LPAREN and pop token
+
+        params = self.parameters()  # list of parameters of the function
+
+        self.local_parser.match('RPAREN')  # match RPAREN and pop token
+
+        func_call_repr = Rule.FuncCall(name, params)
+        return func_call_repr
+
+    def func_body(self):
+        name = self.local_parser.match('NAME')  # peek at the next element
+
+        self.local_parser.match('LPAREN')  # match LPAREN and pop token
+
+        params = self.parameters()  # list of parameters of the function
+
+        self.local_parser.match('RPAREN')  # match RPAREN and pop token
+
+        func_body_repr = Rule.FuncBody(name, params)
+        return func_body_repr
+
+    def expression(self):
+        """expression = name / plus /integer"""
+        start = self.local_parser.peek()  # peek at current element
+        try:
+            if start == 'NAME':  # if it is a name
+                name = self.local_parser.match('NAME')  # match NAME and pops it
+                if self.local_parser.peek() == 'PLUS':  # if next element is PLUS
+                    return self.plus(name)  # as parameter is given the left side of the addition
+                else:
+                    return name  # return name
+            elif start == 'INTEGER':  # if it is integer
+                number = self.local_parser.match('INTEGER')  # match the integer and pop the token
+                number_repr = Rule.Integer(number)
+                if self.local_parser.peek() == 'PLUS':  # if next token is PLUS
+                    return self.plus(number_repr)  # as parameter is given the left argument of the addition
+                else:
+                    return number_repr
+        except SyntaxError:
+            print('Syntax error %r' % start)
+
+    def plus(self, left):
+        """plus = expression PLUS expression"""
+        plus = self.local_parser.match('PLUS')  # match + and pop PLUS token
+        right = self.expression()  # get the right element of the addition
+        plus_repr = Rule.Plus(plus, left, right)
+        return plus_repr
+
+    def main(self):
+        results = []
+        try:
+            while not self.local_parser.done():
+                results.append(self.root())  # go 1 time, 2 times, 3 times
+        except LookupError:
+            print('Index Error Exception Raised, list index out of range"')
+        else:
+            return results
